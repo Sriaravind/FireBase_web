c0d5586a86cca3c845cd8a1005d18c16df835bba
Sriaravind <33258528+Sriaravind@users.noreply.github.com>
2019-03-01 15:53:01 +0530

    Create fireBase.js

---
+++ fireBase.js
@@ -0,0 +1,13 @@
+(function(){
+    var config = {
+        apiKey: "AIzaSyDMBz6TSbJf4Ff4dO_FPKMlZY1VaWsov6k",
+        authDomain: "samplelogin-bfcd4.firebaseapp.com",
+        databaseURL: "https://samplelogin-bfcd4.firebaseio.com",
+        projectId: "samplelogin-bfcd4",
+        storageBucket: "samplelogin-bfcd4.appspot.com",
+        messagingSenderId: "483180701435"
+      };
+      firebase.initializeApp(config);
+})()
+  app_fireBase = firebase;
+  
